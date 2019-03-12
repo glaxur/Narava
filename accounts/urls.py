d4a16d7576b30b50d18a790f6d16b579b9c04763
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-03-11 21:14:00 -0400

    fixed login not working, started the overall layout

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,7 +8,7 @@ from accounts.views import AccountIndex
 app_name = "users"
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='registration/login.html'), name="login"),
-    path('logout', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('', views.AccountIndex.as_view(), name="account")
 
