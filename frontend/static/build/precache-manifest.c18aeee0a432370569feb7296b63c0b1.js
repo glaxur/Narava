3949df3ed58fb9a7216a930182d2bb9d97eea6f0
Isaiah Santillan <isaiahsantillan111@icloud.com>
2019-03-26 15:50:09 -0400

    added build

---
+++ frontend/static/build/precache-manifest.c18aeee0a432370569feb7296b63c0b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "73b8226be3b77b7cd37e",
+    "url": "/static/css/main.980eda82.chunk.css"
+  },
+  {
+    "revision": "73b8226be3b77b7cd37e",
+    "url": "/static/js/main.3c9cfb03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c72adf978d70394f6e28",
+    "url": "/static/js/2.06eb647d.chunk.js"
+  },
+  {
+    "revision": "cf7959e365594ef722425ec774e5ddbc",
+    "url": "/static/media/thomas-kelley-64329-unsplash copy.cf7959e3.jpg"
+  },
+  {
+    "revision": "c0db21722982be734c938783c24b9084",
+    "url": "/index.html"
+  }
+];
